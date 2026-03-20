64bc26e6b96d90a7c24bd4f0eb8ba6c450cc3b9f
Ha1zake <roma.nikolaenko.07@mail.ru>
2026-03-20 19:36:57 +0700

    добавлена грид зона, меню кнопок, новые ветки

--- app/src/main/java/ru/tsu/mobileprojectmap/ui/screens/map/MapUIState.kt
+++ app/src/main/java/ru/tsu/mobileprojectmap/ui/screens/map/MapUIState.kt
@@ -1,15 +1,2 @@
 package ru.tsu.mobileprojectmap.ui.screens.map
-import ru.tsu.mobileprojectmap.domain.model.Point
-import ru.tsu.mobileprojectmap.ui.screens.map.model.MapCell
-import ru.tsu.mobileprojectmap.ui.screens.map.model.MapEditMode
 
-data class MapUIState(
-    val rows: Int = 130,
-    val cols: Int = 130,
-    val cells: List<List<MapCell>> = emptyList(),
-    val currentMode: MapEditMode = MapEditMode.SET_OBSTACLE,
-    val startCell: MapCell? = null,
-    val finishCell: MapCell? = null,
-    val path: List<Point> = emptyList(),
-    val message: String? = null
-    )
