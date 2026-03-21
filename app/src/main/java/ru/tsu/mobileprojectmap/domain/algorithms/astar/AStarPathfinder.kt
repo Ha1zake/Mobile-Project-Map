cf66d2c0fd03b832d10f4569b1e793acd128f95b
Valentin <kutonovv@gmail.com>
2026-03-21 23:53:52 +0700

    Рабочая версия алгоритма A*

--- app/src/main/java/ru/tsu/mobileprojectmap/domain/algorithms/astar/AStarPathfinder.kt
+++ app/src/main/java/ru/tsu/mobileprojectmap/domain/algorithms/astar/AStarPathfinder.kt
@@ -3,8 +3,17 @@ package ru.tsu.mobileprojectmap.domain.algorithms.astar
 import ru.tsu.mobileprojectmap.domain.model.GridCell
 import ru.tsu.mobileprojectmap.domain.model.Point
 import java.util.PriorityQueue
+import kotlin.math.abs
+import kotlin.math.min
 
 class AStarPathfinder {
+
+    private fun heuristic(a: Point, b: Point) : Double {
+        val dx = abs(a.x - b.x)
+        val dy = abs(a.y - b.y)
+        val dist = dx + dy + (1.4 - 2) * min(dx, dy)
+        return dist
+    }
 
     fun findPath(
         grid : List<GridCell>,
@@ -17,10 +26,24 @@ class AStarPathfinder {
         val startNode = Node(
             point = start,
             g = 0.0,
-            h = 0.0
+            h = heuristic(start, end)
         )
 
         openSet.add(startNode)
+
+        val directions = listOf(
+            Point(1, 0),
+            Point(-1, 0),
+            Point(0, 1),
+            Point(0, -1),
+            Point(1, 1),
+            Point(1, -1),
+            Point(-1, 1),
+            Point(-1, -1)
+        )
+
+        val nodeMap = mutableMapOf<Point, Node>()
+        nodeMap[start] = startNode
 
         while (openSet.isNotEmpty()) {
             val current = openSet.poll()
@@ -30,19 +53,6 @@ class AStarPathfinder {
             }
 
             closedSet.add(current.point)
-
-            val directions = listOf(
-                Point(1, 0),
-                Point(-1, 0),
-                Point(0, 1),
-                Point(0, -1),
-                Point(1, 1),
-                Point(1, -1),
-                Point(-1, 1),
-                Point(-1, -1)
-            )
-
-            val nodeMap = mutableMapOf<Point, Node>()
 
             for (dir in directions) {
                 val neighbor = Point(
@@ -55,6 +65,14 @@ class AStarPathfinder {
 
                 if (cell == null) continue
 
+                if (dir.x != 0  && dir.y != 0) {
+                    val first =
+                        grid.find { (it.point.x == current.point.x && it.point.y == neighbor.y) }
+                    val second =
+                        grid.find { (it.point.x == neighbor.x && it.point.y == current.point.y) }
+                    if (first?.isWalkable != true || second?.isWalkable != true) continue
+                }
+
                 if (!cell.isWalkable) continue
 
                 val neighborNode = nodeMap[neighbor] ?: Node(point = neighbor)
@@ -66,7 +84,7 @@ class AStarPathfinder {
                 if (newG < neighborNode.g) {
                     neighborNode.g = newG
                     neighborNode.parent = current
-                    neighborNode.h = 0.0
+                    neighborNode.h = heuristic(neighborNode.point, end)
                     nodeMap[neighbor] = neighborNode
                     openSet.add(neighborNode)
                 }
