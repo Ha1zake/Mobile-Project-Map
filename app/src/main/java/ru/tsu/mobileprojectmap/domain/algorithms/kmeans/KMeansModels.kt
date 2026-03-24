c4ff93d1aff6d33ac53f6a0e9d8ee0fb72c6c175
kyoshi <give_me_gg@mail.ru>
2026-03-24 23:40:43 +0700

    cluster algorithm

--- app/src/main/java/ru/tsu/mobileprojectmap/domain/algorithms/kmeans/KMeansModels.kt
+++ app/src/main/java/ru/tsu/mobileprojectmap/domain/algorithms/kmeans/KMeansModels.kt
@@ -1,4 +1,32 @@
 package ru.tsu.mobileprojectmap.domain.algorithms.kmeans
 
-class KMeansModels {
-}
+data class ClusterPoint (
+    val id: String,
+    val x: Double,
+    val y: Double,
+    val name: String? = null
+)
+
+data class Centroid (
+    val x: Double,
+    val y: Double
+)
+
+data class Cluster(
+    val id: Int,
+    val centroid: Centroid,
+    val points: List<ClusterPoint>
+)
+
+data class KMeansInput(
+    val points: List<ClusterPoint>,
+    val k: Int,
+    val maxIterations: Int = 100,
+    val tolerance: Double = 0.001
+)
+
+data class KMeansResult (
+    val clusters: List<Cluster>,
+    val iterations: Int,
+    val converged: Boolean
+)
