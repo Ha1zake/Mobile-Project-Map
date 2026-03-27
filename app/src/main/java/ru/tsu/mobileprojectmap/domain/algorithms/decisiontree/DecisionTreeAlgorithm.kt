91818a305f04c9fa52d8bc5cee10293f1e7e00fe
kyoshi <give_me_gg@mail.ru>
2026-03-27 19:50:19 +0700

    add file into usecase for ui build

--- app/src/main/java/ru/tsu/mobileprojectmap/domain/algorithms/decisiontree/DecisionTreeAlgorithm.kt
+++ app/src/main/java/ru/tsu/mobileprojectmap/domain/algorithms/decisiontree/DecisionTreeAlgorithm.kt
@@ -1,2 +1,7 @@
 package ru.tsu.mobileprojectmap.domain.algorithms.decisiontree
 
+object DecisionTreeAlgorithm {
+    fun biuld(sample: List<TrainingSample>): DecisionTreeResult {
+        TODO("построение дерева")
+    }
+}
--- app/src/main/java/ru/tsu/mobileprojectmap/domain/algorithms/decisiontree/DecisionTreeCsvParser.kt
+++ app/src/main/java/ru/tsu/mobileprojectmap/domain/algorithms/decisiontree/DecisionTreeCsvParser.kt
@@ -1,2 +1,7 @@
 package ru.tsu.mobileprojectmap.domain.algorithms.decisiontree
 
+object DecisionTreeCsvParser {
+    fun parse(csvText: String): List<TrainingSample> {
+        TODO("написать парсилку для csv текста")
+    }
+}
