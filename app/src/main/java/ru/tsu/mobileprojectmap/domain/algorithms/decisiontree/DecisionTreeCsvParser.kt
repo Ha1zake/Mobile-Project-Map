316ea0ac448c897b1efe7a9a805941951c693ddf
kyoshi <give_me_gg@mail.ru>
2026-03-27 20:58:26 +0700

    add csv parser

--- app/src/main/java/ru/tsu/mobileprojectmap/domain/algorithms/decisiontree/DecisionTreeCsvParser.kt
+++ app/src/main/java/ru/tsu/mobileprojectmap/domain/algorithms/decisiontree/DecisionTreeCsvParser.kt
@@ -2,6 +2,36 @@ package ru.tsu.mobileprojectmap.domain.algorithms.decisiontree
 
 object DecisionTreeCsvParser {
     fun parse(csvText: String): List<TrainingSample> {
-        TODO("написать парсилку для csv текста")
+
+        val lines = csvText.lines()
+            .map{it.trim()}
+            .filter { it.isNotEmpty() }
+
+        if (lines.isEmpty()) {
+            throw IllegalArgumentException("csv пуст")
+        }
+        val header = lines.first().split(",")
+        val datalines = lines.drop(1)
+
+        return datalines.map { lines ->
+            val values = lines.split(",")
+
+            if (values.size != header.size) {
+                throw IllegalArgumentException("неверный формат CSV")
+            }
+
+            val featureName = header.dropLast(1)
+            val featureValues = header.drop(1)
+
+            val feature = featureName.zip(featureValues).toMap()
+            val label = values.last()
+
+            TrainingSample (
+                features = feature,
+                label = label,
+            )
+
+        }
+
     }
 }
