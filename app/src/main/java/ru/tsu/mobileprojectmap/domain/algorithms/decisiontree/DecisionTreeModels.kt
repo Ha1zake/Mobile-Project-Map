643b751902ccf0582e96eb5d9ab3a7ffd3b42984
kyoshi <give_me_gg@mail.ru>
2026-03-27 17:11:24 +0700

    add models for Tree

---
+++ app/src/main/java/ru/tsu/mobileprojectmap/domain/algorithms/decisiontree/DecisionTreeModels.kt
@@ -0,0 +1,29 @@
+package ru.tsu.mobileprojectmap.domain.algorithms.decisiontree
+
+//обучающая структура
+data class TrainingSample (
+    val features: Map<String, String>,
+    val label: String,
+)
+
+//Узлы дерева
+sealed class DecisionTreeNode {
+
+    data class DecisionNode(
+    val featureName: String,
+    val branches: Map<String, DecisionTreeNode>
+    ): DecisionTreeNode()
+
+    data class LeafNode(
+        val label: String
+    ): DecisionTreeNode()
+}
+
+data class DecisionTreeResult(
+    val root: DecisionTreeNode
+)
+
+data class PredictionResult (
+    val predictedLabel: String,
+    val decisionPath: List<String>,
+)
