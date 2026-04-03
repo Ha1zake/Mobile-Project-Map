e82a2e8a62ccbfc355415a98fc4fa51c0cd06c2f
kyoshi <give_me_gg@mail.ru>
2026-04-03 20:36:01 +0700

    feat: add hepler funs into TreeAlgorithm

--- app/src/main/java/ru/tsu/mobileprojectmap/domain/algorithms/decisiontree/DecisionTreeAlgorithm.kt
+++ app/src/main/java/ru/tsu/mobileprojectmap/domain/algorithms/decisiontree/DecisionTreeAlgorithm.kt
@@ -2,6 +2,44 @@ package ru.tsu.mobileprojectmap.domain.algorithms.decisiontree
 
 object DecisionTreeAlgorithm {
     fun biuld(sample: List<TrainingSample>): DecisionTreeResult {
-        TODO("построение дерева")
+        require(sample.isNotEmpty()) {"Нету данных"}
+
+        val availableFeatures = sample.first().features.keys.toList()
+        val root = buildNode(sample, availableFeatures)
+
+        return DecisionTreeResult(root)
     }
+
+    private fun buildNode(
+        sample: List<TrainingSample>,
+        availableFeatures: List<String>
+    ): DecisionTreeNode {
+        TODO();
+    }
+
+    private fun allSamplesHaveSameLabel(sample: List<TrainingSample>): Boolean {
+        return sample.map { it.label }.distinct().size == 1
+    }
+
+    private fun getMostCommonLabel(samples: List<TrainingSample>): String {
+        return samples.groupingBy { it.label }
+            .eachCount()
+            .maxByOrNull { it.value }!!
+            .key
+    }
+
+    private fun groupByFeature (
+        sample: List<TrainingSample>,
+        featureName: String
+    ): Map<String, List<TrainingSample>> {
+        return sample.groupBy { it.features[featureName] ?: "Неизвестный" }
+    }
+
+    private fun chooseBestFeature(
+        sample: List<TrainingSample>,
+        availableFeatures: List<String>
+    ): String {
+        return availableFeatures.first()
+    }
+
 }
