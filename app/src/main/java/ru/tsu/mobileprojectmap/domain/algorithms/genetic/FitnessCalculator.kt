7f19464e5277df124c0cd7ae8ae0de5be33e75e3
Valentin <kutonovv@gmail.com>
2026-04-15 21:46:24 +0700

    start genetic alg

---
+++ app/src/main/java/ru/tsu/mobileprojectmap/domain/algorithms/genetic/FitnessCalculator.kt
@@ -0,0 +1,10 @@
+package ru.tsu.mobileprojectmap.domain.algorithms.genetic
+
+class FitnessCalculator {
+    fun calculate(
+        individual: Individual,
+        data: Double
+    ) : Double {
+        TODO()
+    }
+}
